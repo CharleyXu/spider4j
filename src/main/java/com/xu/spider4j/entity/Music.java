aadeaace6c1490318ed48bb72ff0b22298ef7693
CharleyXu <xucharlie1993@gmail.com>
2017-11-21 19:34:58 +0800

    更新Mapper类

--- src/main/java/com/xu/spider4j/entity/Music.java
+++ src/main/java/com/xu/spider4j/entity/Music.java
@@ -8,7 +8,7 @@ import com.alibaba.fastjson.JSON;
 public class Music {
 	private int	id;//歌曲Id
 	private String	name;//名称
-	private String	artistsId;//歌手Id( 有 邵夷贝 / GALA 这种情况)
+//	private String	artistsId;//歌手Id( 有 邵夷贝 / GALA 这种情况)	多对多，建立关系表
 	private int score;//热度
 	private String	album;//专辑名称
 	private String commentThreadId;//评论ThreadId
@@ -16,10 +16,9 @@ public class Music {
 	public Music() {
 	}
 
-	public Music(int id, String name, String artistsId, int score, String album, String commentThreadId) {
+	public Music(int id, String name, int score, String album, String commentThreadId) {
 		this.id = id;
 		this.name = name;
-		this.artistsId = artistsId;
 		this.score = score;
 		this.album = album;
 		this.commentThreadId = commentThreadId;
@@ -39,14 +38,6 @@ public class Music {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getArtistsId() {
-		return artistsId;
-	}
-
-	public void setArtistsId(String artistsId) {
-		this.artistsId = artistsId;
 	}
 
 	public int getScore() {
--- src/main/java/com/xu/spider4j/pipeline/NetEaseCloudMusicPipeline.java
+++ src/main/java/com/xu/spider4j/pipeline/NetEaseCloudMusicPipeline.java
@@ -8,6 +8,7 @@ import com.xu.spider4j.entity.Music;
 import com.xu.spider4j.entity.RelationArtlistMusic;
 import com.xu.spider4j.mapper.ArtistMapper;
 import com.xu.spider4j.mapper.MusicMapper;
+import com.xu.spider4j.mapper.RelationArtlistMusicMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,6 +28,8 @@ public class NetEaseCloudMusicPipeline implements Pipeline{
 	private ArtistMapper artistMapper;
 	@Autowired
 	private MusicMapper musicMapper;
+	@Autowired
+	private RelationArtlistMusicMapper relationArtlistMusicMapper;
 
 	// ResultItems保存了抽取结果，它是一个Map结构，
 	// 在page.putField(key,value)中保存的数据，可以通过ResultItems.get(key)获取
@@ -34,51 +37,37 @@ public class NetEaseCloudMusicPipeline implements Pipeline{
 	public void process(ResultItems resultItems, Task task) {
 		JSONArray jsonArray = JSONArray.parseArray(resultItems.get("result"));
 		System.out.println("size:"+jsonArray.size()+"\n"+jsonArray.toJSONString());
-
-
-		//处理歌手信息
-		JSONArray array = jsonArray.getJSONObject(0).getJSONArray("artists");
-		List<Artist> artistList = JSON.parseArray(array.toString(), Artist.class);
-
-		System.out.println("-----artistList-----:"+artistList.size()+"\n"+artistList);
-//		artistMapper.insert(artist);
-
+		//处理歌手信息	artistList
+		List<Artist> artistList = new ArrayList<>();
 		//处理音乐信息	批量
 		List<Music> musicList = new ArrayList<>();
-
 		//处理歌手_音乐关系_信息
 		List<RelationArtlistMusic> relationArtlistMusicList = new ArrayList<>();
 
 		for (int i = 0,size = jsonArray.size(); i <size ; i++) {
 			JSONObject jsonObject = jsonArray.getJSONObject(i);
-			int songId = jsonObject.getInteger("id");//音乐Id
+			int musicId = jsonObject.getInteger("id");//音乐Id
 			String name = jsonObject.getString("name");//音乐名称
 			int score = jsonObject.getInteger("score");//热度
 			String album = jsonObject.getJSONObject("album").getString("name");//专辑名称
 			String commentThreadId = jsonObject.getString("commentThreadId");//评论请求Id
 
 			JSONArray artists = jsonObject.getJSONArray("artists");
-			int artistSize = artists.size();
-			String artistId = null;//歌手Id
-			if(artistSize<2){
-				artistId = artists.getJSONObject(0).getInteger("id").toString();
-			}else{
-				StringBuilder stringBuilder = new StringBuilder();
-				for (int j = 0; j < artistSize; j++) {
-					int sid = artists.getJSONObject(j).getInteger("id");
-					if (sid!=0){
-						stringBuilder.append(artists.getJSONObject(j).getInteger("id")+",");
-					}
-					if (j==artistSize-1){
-						stringBuilder.deleteCharAt(stringBuilder.length()-1);
-					}
-				}
-				artistId = stringBuilder.toString();
+
+			List<Artist> tempList = JSON.parseArray(artists.toJSONString(), Artist.class);//临时ArtistList
+			for (int j = 0,length = tempList.size(); j < length; j++) {
+				int artistId = tempList.get(j).getId();
+				RelationArtlistMusic relationArtlistMusic = new RelationArtlistMusic(artistId,musicId);
+				relationArtlistMusicList.add(relationArtlistMusic);
 			}
-			Music music = new Music(songId,name,artistId,score,album,commentThreadId);
+			artistList.addAll(tempList);
+
+			Music music = new Music(musicId,name,score,album,commentThreadId);
 			System.out.println(music);
 			musicList.add(music);
 		}
-//		musicMapper.batchInsert(musicList);
+		//		artistMapper.batchInsert(artist);
+		//		musicMapper.batchInsert(musicList);
+
 	}
 }
